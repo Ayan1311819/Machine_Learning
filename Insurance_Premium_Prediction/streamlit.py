40a7ab4d59a8dabbbaa74733dabf8e7b608b3827
Ayan1311819 <ayanchoudhary040@gmail.com>
2024-07-24 14:13:18 +0530

    add path to my_model.joblib

--- Insurance_Premium_Prediction/streamlit.py
+++ Insurance_Premium_Prediction/streamlit.py
@@ -3,7 +3,11 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import joblib
 # Load the trained model
-model = joblib.load('my_model.joblib')  # Replace with the actual path to your trained model
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Construct the path to the model file
+model_path = os.path.join(current_dir, 'my_model.joblib')
+model = joblib.load(model_path)  # Replace with the actual path to your trained model
 
 def run_streamlit():
     st.title('Insurance Premium Prediction')
