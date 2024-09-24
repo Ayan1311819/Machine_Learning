cb48d08c59483eb5c933a7cc51615aa23f40b1c3
Ayan1311819 <ayanchoudhary040@gmail.com>
2024-09-25 01:30:51 +0530

    Update streamlit.py

--- CNN/streamlit.py
+++ CNN/streamlit.py
@@ -2,12 +2,11 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 import numpy as np
+import gdown
+import os
 
-file_id = '1-AQGIP_4wIGacaTcHmNg0WdbzvXFwk8W'
-url = f'https://drive.google.com/uc?id={file_id}'
-output = 'Amodel.h5'
-gdown.download(url, output, quiet=False, use_cookies=False)
-model = tf.keras.models.load_model('my_model.h5')
+
+model = tf.keras.models.load_model('Amodel_full.h5')
 
 mclass = ['Corn: Common Rust',
   'Corn: Gray Leaf Spot',
@@ -37,8 +36,6 @@ uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=300)
-    st.write("")
     st.write("Classifying...")
     processed_image = preprocess_image(image)
     prediction = model.predict(processed_image)
